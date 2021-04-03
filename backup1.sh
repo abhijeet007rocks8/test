744558e5a23d6b11a8a78ddedd5601b782eedbfa
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-04-03 09:42:38 +0530

    Auto-Commit at : Sat Apr  3 09:42:38 IST 2021

---
+++ backup1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+path="home/abhijeet/test/test/" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder  
+while true 
+do
+		inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path
+			echo "$path"   
+			    cd $path                              #> /dev/null &> /dev/null
+			    	git pull                              #> /dev/null &> /dev/null
+					git add --all                         #> /dev/null &> /dev/null
+						now=$(date)                           #> /dev/null &> /dev/null
+							git commit -m "Auto-Commit at : $now" #> /dev/null &> /dev/null	
+								git push -u origin master             #> /dev/null &> /dev/null
+								    sleep 30
+							    done
